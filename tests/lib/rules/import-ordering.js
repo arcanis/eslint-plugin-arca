51b7ed86268a2180d6dfc2c1d9db73d825ca4370
Maël Nison <nison.mael@gmail.com>
2020-12-25 16:45:38 +0100

    Revert "Prototypes multi-fix"
    This reverts commit 560bd9428ebd854a6954a998e6abf002290c4898.

--- tests/lib/rules/import-ordering.js
+++ tests/lib/rules/import-ordering.js
@@ -27,7 +27,7 @@ ruleTester.run("import-ordering", rule, {
     valid: [
 
         {
-            code: "import 'foo';\nimport bar1 from 'bar/bar';\nimport bar2 from 'bar';\nimport foo1 from 'foo/foo';\nimport foo2 from 'foo';\nimport bar3 from 'common/bar';\nimport foo3 from 'common/foo';\nimport bar4 from 'app/bar/bar';\nimport foo4 from 'app/bar/foo';\nimport bar5 from 'app/foo/bar';\nimport foo5 from 'app/foo/foo';\n",
+            code: "import 'foo';\nimport bar1 from 'bar/bar';\nimport bar2 from 'bar';\nimport foo1 from 'foo/foo';\nimport foo2 from 'foo';\n\nimport bar3 from 'common/bar';\nimport foo3 from 'common/foo';\n\nimport bar4 from 'app/bar/bar';\nimport foo4 from 'app/bar/foo';\nimport bar5 from 'app/foo/bar';\nimport foo5 from 'app/foo/foo';\n",
             parserOptions: parserOptions
         }
 
@@ -42,14 +42,14 @@ ruleTester.run("import-ordering", rule, {
             errors: [{ message: "Expected 'foo' to be imported before 'bar' (side-effects go first)." }]
         },
         {
-            code: "import foo from 'common/foo';\nimport bar from 'bar';\n",
-            output: "import bar from 'bar';\nimport foo from 'common/foo';\n",
+            code: "import foo from 'common/foo';\n\nimport bar from 'bar';\n",
+            output: "import bar from 'bar';\n\nimport foo from 'common/foo';\n",
             parserOptions: parserOptions,
             errors: [{ message: "Expected 'bar' to be imported before 'common/foo' (vendors go first)." }]
         },
         {
-            code: "import foo from 'app/foo';\nimport bar from 'common/bar';\n",
-            output: "import bar from 'common/bar';\nimport foo from 'app/foo';\n",
+            code: "import foo from 'app/foo';\n\nimport bar from 'common/bar';\n",
+            output: "import bar from 'common/bar';\n\nimport foo from 'app/foo';\n",
             parserOptions: parserOptions,
             errors: [{ message: "Expected 'common/bar' to be imported before 'app/foo' ('^common/' goes before '^app/')." }]
         },
