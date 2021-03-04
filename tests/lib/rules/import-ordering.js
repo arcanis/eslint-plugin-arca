546c0e28e6e3855349c8e0580fbfec5a961bd593
Benjamin Koltes <benjamin.koltes@datadoghq.com>
2021-03-04 18:51:51 +0100

    [import-ordering] add test for stable sort

--- tests/lib/rules/import-ordering.js
+++ tests/lib/rules/import-ordering.js
@@ -29,6 +29,11 @@ ruleTester.run("import-ordering", rule, {
         {
             code: "import 'foo';\nimport bar1 from 'bar/bar';\nimport bar2 from 'bar';\nimport foo1 from 'foo/foo';\nimport foo2 from 'foo';\n\nimport bar3 from 'common/bar';\nimport foo3 from 'common/foo';\n\nimport bar4 from 'app/bar/bar';\nimport foo4 from 'app/bar/foo';\nimport bar5 from 'app/foo/bar';\nimport foo5 from 'app/foo/foo';\n",
             parserOptions: parserOptions
+        },
+        {
+            // When the path is the same, keep user's order
+            code: "import {bar1} from 'bar';\nimport {bar2} from 'bar';\nimport baz from 'baz';\nimport {foo2} from 'foo';\nimport {foo1} from 'foo';",
+            parserOptions: parserOptions
         }
 
     ],
