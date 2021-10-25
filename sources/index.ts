280c01e4173001b857c1ebff277ec97ac4fb947e
Maël Nison <nison.mael@gmail.com>
2021-10-25 23:46:32 +0200

    Fixes index

--- sources/index.ts
+++ sources/index.ts
@@ -10,6 +10,7 @@ import importAbsolutesRule           from './rules/import-absolutes';
 import importAlignRule               from './rules/import-align';
 import importOrderingRule            from './rules/import-ordering';
 import importQuotesRule              from './rules/import-quotes';
+import jsxLonghandPropsRule          from './rules/jsx-longhand-props';
 import meltedConstructsRule          from './rules/melted-constructs';
 import newlineAfterImportSectionRule from './rules/newline-after-import-section';
 import noDefaultExportRule           from './rules/no-default-export';
@@ -20,6 +21,7 @@ export const rules = {
   [`import-align`]: importAlignRule,
   [`import-ordering`]: importOrderingRule,
   [`import-quotes`]: importQuotesRule,
+  [`jsx-longhand-props`]: jsxLonghandPropsRule,
   [`melted-constructs`]: meltedConstructsRule,
   [`newline-after-import-section`]: newlineAfterImportSectionRule,
   [`no-default-export`]: noDefaultExportRule,
